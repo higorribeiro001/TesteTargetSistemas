f25db0534e196f2b3a12b366729f4fdf727eff28
Higor Araujo <higor.araujo@nees.ufal.br>
2024-09-07 11:59:16 -0300

    first commit

---
+++ QuestaoTres/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// questao 3
+// considerando que 2024 tem 254 dias úteis
+Random random = new Random();
+List<decimal> billingDays = new List<decimal>();
+
+for(int i=0; i<254; i++) {
+    billingDays.Add((decimal)random.NextDouble() * 1000000);
+}
+
+decimal LowestValue(List<decimal> invoicing) {
+    return invoicing.Min();
+}
+
+decimal GreaterValue(List<decimal> invoicing) {
+    return invoicing.Max();
+}
+
+int DaysValueHigherThanAverage(List<decimal> invoicing) {
+    decimal mean = invoicing.Sum()/invoicing.Count;
+    int days = 0;
+    foreach (int value in invoicing) {
+        if (value > mean) {
+            days++;
+        }
+    }
+
+    return days;
+}
+
+Console.WriteLine($"Menor faturamento: {LowestValue(billingDays).ToString("c")}.");
+Console.WriteLine($"Maior faturamento: {GreaterValue(billingDays).ToString("c")}.");
+Console.WriteLine($"Números de dias em que o faturamento foi maior que a média anual: {DaysValueHigherThanAverage(billingDays)}.");
+
+// Exemplo de resultado:
+// Menor faturamento: R$ 812,87.
+// Maior faturamento: R$ 999.458,98.
+// Números de dias em que o faturamento foi maior que a média anual: 123.
---
+++ QuestaoUm/Program.cs
@@ -0,0 +1,12 @@
+﻿// questão 1
+
+int INDICE = 12, SOMA = 0, K = 1;
+
+while(K < INDICE) {
+    K += 1; 
+    SOMA += K;
+} 
+
+Console.WriteLine(SOMA);
+
+// o valor da variavel SOMA será 77 
